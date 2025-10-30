6e7d8486e232f2d95e6dce438c90a87caca5f214
Shad Storhaug <shad@shadstorhaug.com>
2025-10-30 21:14:43 +0700

    SpanTools.ValueStringBuilder.Generator.Tests.csproj: Added fallback for pre-build stage so the project will build even though there is not yet a package to build against in the graph. This generates a single failing test to ensure this condition isn't mistakenly used to run tests. It should be a temporary condition that rights itself later.

---
+++ tests/SpanTools.ValueStringBuilder.Generator.Tests/TestPackageNotBuilt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SpanTools.Generator.Tests
+{
+    public class TestPackageNotBuilt
+    {
+        public void NoGeneratedContent()
+        {
+            Assert.Fail("The generator package was not referenced during this build. Ensure the NuGet package was built and $(_PackageVersionPropsFilePath) exists.");
+        }
+    }
+}
